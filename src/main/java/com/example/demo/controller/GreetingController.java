cfe23f702ee2ce421893a1a787074b9bd63078e7
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-19 05:56:33 +0530

    Added:Code to add post mapping in Greeting App.

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.Greeting;
+import com.example.demo.model.User;
+import com.example.demo.service.IGreetingService;
+
+@RestController
+public class GreetingController {
+	private static final String template="Hello, %s!";
+	private final AtomicLong counter=new AtomicLong();
+	
+	@Autowired
+	private IGreetingService GreetingService;
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name) {
+		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+	}
+	
+	@GetMapping("/greetingMsg")
+	public String greetingMsg(String msg) {
+		return GreetingService.message(msg);
+	}
+	
+	@PostMapping("/greetingWithName")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!!!";
+	}
+}
